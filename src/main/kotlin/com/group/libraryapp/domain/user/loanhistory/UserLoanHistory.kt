d6422a42650f5560e59c1af9d87aeaaf2618ff90
uknow-home <yoon6763@naver.com>
2024-02-07 19:03:20 +0900

    boolean에 enum 적용

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.kt
@@ -13,13 +13,29 @@ class UserLoanHistory(
     @ManyToOne
     val user: User,
     val bookName: String,
-    var isReturn: Boolean = false,
+    var status: UserLoanStatus = UserLoanStatus.LOANED,
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
 ) {
 
     fun doReturn() {
-        isReturn = true
+        status = UserLoanStatus.RETURNED
     }
 
+
+    companion object {
+        fun fixture(
+            user: User,
+            bookName: String = "이상한 나라의 앨리스",
+            status: UserLoanStatus = UserLoanStatus.LOANED,
+            id: Long? = null,
+        ): UserLoanHistory {
+            return UserLoanHistory(
+                user = user,
+                bookName = bookName,
+                status = status,
+                id = id,
+            )
+        }
+    }
 }
--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -7,6 +7,7 @@ import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
@@ -63,7 +64,7 @@ class BookServiceTest @Autowired constructor(
         Assertions.assertThat(results).hasSize(1)
         Assertions.assertThat(results[0].user.name).isEqualTo("크오옹")
         Assertions.assertThat(results[0].bookName).isEqualTo("이상한 나라의 앨리스")
-        Assertions.assertThat(results[0].isReturn).isFalse
+        Assertions.assertThat(results[0].status).isEqualTo(UserLoanStatus.LOANED)
     }
 
     @Test
@@ -72,7 +73,7 @@ class BookServiceTest @Autowired constructor(
         // given
         bookRepository.save(Book.fixture(name = "이상한 나라의 앨리스"))
         val savedUser = userRepository.save(User("크오옹", null))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스", false))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스"))
         val request = BookLoanRequest("크오옹", "이상한 나라의 앨리스")
 
         // when & then
@@ -86,7 +87,7 @@ class BookServiceTest @Autowired constructor(
         // given
         bookRepository.save(Book.fixture())
         val savedUser = userRepository.save(User("크오옹", null))
-        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스", false))
+        userLoanHistoryRepository.save(UserLoanHistory(savedUser, "이상한 나라의 앨리스"))
         val request = BookReturnRequest("크오옹", "이상한 나라의 앨리스")
 
         // when
@@ -95,7 +96,7 @@ class BookServiceTest @Autowired constructor(
         // then
         val results = userLoanHistoryRepository.findAll()
         Assertions.assertThat(results).hasSize(1)
-        Assertions.assertThat(results[0].isReturn).isTrue
+        Assertions.assertThat(results[0].status).isEqualTo(UserLoanStatus.RETURNED)
     }
 
 }
